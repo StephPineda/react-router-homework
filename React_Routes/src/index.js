ca8319cdd73602af7662680dc1de61f3866f72e1
Stephanie Pineda <stefoswimo@hotmail.com>
2019-05-09 17:48:30 -0400

    react route

--- React_Routes/src/index.js
+++ React_Routes/src/index.js
@@ -1,18 +1,67 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './CSS/index.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Happy from './components/Happy';
 import Sleepy from './components/Sleepy';
 import Guilty from './components/Guilty';
 
-// your code goes here
-class Home extends Component{
-  render(){
-    return(
-      // your code goes here
+
+
+class Dogs extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/happy">Happy</Link>
+            </li>
+            <li>
+              <Link to="/about">Sleepy</Link>
+            </li>
+            <li>
+              <Link to="/topics">Guilty</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Route exact path="/happy" component={Happy} />
+          <Route path="/Sleepy" component={Sleepy} />
+          <Route path="/Guilty" component={Guilty} />
+          <Route path="/" component={Home} />
+        </div>
+      </Router>
+
     )
   }
 }
 
-ReactDOM.render( <Home />, document.getElementById('root') );
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ReactDOM.render(<Dogs />, document.getElementById('root'));
